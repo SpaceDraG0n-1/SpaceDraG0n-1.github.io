836d2f7939cb707a85419b54083bc68ed769f1b6
SpaceDraG0n-1 <1411827358@qq.com>
2025-08-08 16:42:44 +0800

    Site updated: 2025-08-08 16:42:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16108.html","posts/16108.html"];function toRandomPost(){
+var posts=["posts/16109.html","posts/16108.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

72bd587d0bde4e4057a5ae23680b8bd298d16243
SpaceDraG0n-1 <1411827358@qq.com>
2025-08-10 19:45:43 +0800

    Site updated: 2025-08-10 19:45:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16109.html","posts/16110.html","posts/16108.html"];function toRandomPost(){
+var posts=["posts/16109.html","posts/16111.html","posts/16110.html","posts/16108.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
